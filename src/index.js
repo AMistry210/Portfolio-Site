64ad8f876c46f0cc1a5ac7060f5b5ccc6d6fe6e7
AMistry210 <116072137+AMistry210@users.noreply.github.com>
2025-03-30 17:20:19 +0100

    Added in orbit controls

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./index.scss"
 import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const canvas = document.querySelector("#experience-canvas");
 const sizes ={
@@ -15,17 +16,38 @@ const renderer = new THREE.WebGLRenderer({canvas:canvas, antialias:true});
 renderer.setSize( sizes.width, sizes.height )
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+camera.position.z = 5
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 )
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5
+// Orbit Controls
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.enableDamping = true
+controls.dampingFactor = 0.05
+controls.update();
+
+// Event Listeners
+window.addEventListener("resize", ()=>{
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update Camera
+    camera.aspect = sizes.width/sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update Renderer
+    renderer.setSize( sizes.width, sizes.height )
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 function animate() {}
 
 const render = () =>{
+    controls.update()
+
     cube.rotation.x += 0.01
     cube.rotation.y += 0.01
   
