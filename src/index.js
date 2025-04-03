6423769eede098031586ff3005ceba9b3909c0d9
AMistry210 <116072137+AMistry210@users.noreply.github.com>
2025-04-03 18:02:49 +0100

    Added a loading screen

--- src/index.js
+++ src/index.js
@@ -382,6 +382,56 @@ function playIntroAnimation(){
     )
 }
 
+// Loading Screen
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function router(route) {
+    const pageName = route ? route : $('.default.page').attr('data-page-name');
+    const $page = $(`[data-page-name="${pageName}"]`);
+    $('.page').css('display', 'none');
+    $('[data-page]').removeClass('active');
+    $(`[data-page="${pageName}"]`).addClass('active');
+    $page.css('display', 'block');
+}
+router();
+
+// Fake loader
+let progress = 0;
+const fakeLoaderInterval = window.setInterval(function() {
+    const $lp = $('.loading-progress');
+    progress = progress + getRandomArbitrary(10, 25);
+    $lp.css('transform', `translateX(${progress}%)`);
+
+    if (progress >= 75) {
+        window.clearInterval(fakeLoaderInterval);
+        $lp.css('transform', 'translateX(100%)');
+
+        setTimeout(() => {
+            $('.loading').css('transform', 'translateY(calc(100% + 10px))');
+        
+            // Wait for the loading screen to fully transition out before playing the intro animation
+            $('.loading').on("transitionend", function() {
+                $(this).remove(); // Optional: remove it from DOM for better performance
+                playIntroAnimation(); // Trigger animations after loading screen is fully gone
+            });
+        }, 400);
+        
+    }
+}, getRandomArbitrary(100, 500));
+
+// Navigation
+$('.main-nav li a').on('click', e => {
+    const $this = $(e.currentTarget);
+    const route = $this.attr('data-page');
+
+    $('.main-nav li a').removeClass('active');
+    $this.addClass('active');
+    router(route);
+});
+
+
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera( 35, sizes.width / sizes.height, 0.1, 1000 )
 
